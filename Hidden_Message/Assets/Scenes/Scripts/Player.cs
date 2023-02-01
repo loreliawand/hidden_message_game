c66481bf453e89e586f7d47cad858bd56f04b15c
loreliawand <loreliawand@gmail.com>
2023-02-01 05:47:45 +0100

    Second HW
    Added player's ball

--- Hidden_Message/Assets/Scenes/Scripts/Player.cs
+++ Hidden_Message/Assets/Scenes/Scripts/Player.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Vector3 mousePosition;
-    float yAxis;
-    float minX = -9f;
-    float maxX = 9f;
+    public GameObject ballPrefab;
+    public GameObject ball;
+
+    private Vector3 mousePosition;
+
+    private float yAxis;
+    private float minX = -9f;
+    private float maxX = 9f;
+    public float ballSpeed = 1.0f;
+
+    private bool pressedKey = false;
+
     void Start()
     {
+        //Y position for player's board
         yAxis = transform.position.y;
+        ball = Instantiate(ballPrefab, new Vector3(0, -3.7f, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -19,5 +28,21 @@ public class Player : MonoBehaviour
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(Mathf.Clamp(mousePosition.x, minX, maxX), yAxis, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space");
+            pressedKey = true;
+        }
+        if (pressedKey == true)
+        {
+            ball.transform.Translate(Vector3.up * ballSpeed * Time.deltaTime);
+        }
+        if (ball.transform.position.y > 6.0f)
+        {
+            //It may be destroy object after reaching conditional position
+            pressedKey = false;
+            ball = Instantiate(ballPrefab, new Vector3(0, -3.7f, 0), Quaternion.identity);
+        }
     }
 }
